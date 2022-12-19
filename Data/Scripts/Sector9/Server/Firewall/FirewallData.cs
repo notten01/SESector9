ca4a8406741ca3c92458ede259e6f1019581d3a8
Nick <48136360+notten01@users.noreply.github.com>
2022-12-19 18:45:24 +0100

    Firewall (#35)
    * added model object and sbc for firewall
    
    * setup lfs tracking
    
    * Added firewall tracking, fixed LOD of model
    
    * Added firewall ticking so the countdown is working
    
    * fixed labeler setup

---
+++ Data/Scripts/Sector9/Server/Firewall/FirewallData.cs
@@ -0,0 +1,23 @@
+﻿namespace Sector9.Server.Firewall
+{
+    public class FirewallData
+    {
+        public long GridId { get; set; }
+        public int FirewallCountdown { get; set; }
+        public bool GameOver { get; set; }
+
+        private const int MissingFirewallTimeout = 432000;
+
+        public FirewallData()
+        {
+            GridId = -1;
+            FirewallCountdown = MissingFirewallTimeout;
+            GameOver = false;
+        }
+
+        public void ResetCountdownn()
+        {
+            FirewallCountdown = MissingFirewallTimeout;
+        }
+    }
+}
--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,3 @@
+(robocopy /MIR Models %appdata%/SpaceEngineers/Mods/Sector9/Models) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+(robocopy /MIR Textures %appdata%/SpaceEngineers/Mods/Sector9/Textures) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
 (robocopy /MIR Data %appdata%/SpaceEngineers/Mods/Sector9/Data) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
